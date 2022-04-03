a1fc2766533098f1ccedf82dd339da3d87290246
拾年 <211154860@qq.com>
2022-04-03 21:04:51 +0800

    😓 补上漏提交的路由文件

--- app/route.php
+++ app/route.php
@@ -5,11 +5,10 @@ Route::get('/',function(){
     return view("index/index");
 });
 
-// Route::get('/','cqhttp@test');
-
 // 处理机器人上报消息
 Route::post('/','cqhttp@index');
 
 Route::get('console','console@index');
 Route::post('console/pluginSetings/save','console@pluginSetingsSave');
+Route::post('console/groupSetings/save','console@groupSetingsSave');
 Route::get('console/getMsg','console@getMsg');
